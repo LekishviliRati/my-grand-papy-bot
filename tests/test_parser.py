a0bd29de0b2d76b455e7929cbb0bc333e0187bc0
LekishviliRati <lekishvili.rati@gmail.com>
2021-05-13 23:52:23 +0200

    parser test correction

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -10,23 +10,13 @@ input_2 = \
     " Est-ce que tu connais l'addresse de la mairie de Paris ? "
 
 
-def test_input_1(input):
+def test_input_1():
     """Test of Input_parser class."""
-    parsed = Input_parser(input)
+    parsed = Input_parser(input_1)
     assert parsed.parsed_input == "adresse openlassrooms"
-    # To see what the function prints
-    # print(parsed.parsed_input)
 
 
-test_input_1(input_1)
-
-
-def test_input_2(input):
+def test_input_2():
     """Test of Input_parser class."""
-    parsed = Input_parser(input)
+    parsed = Input_parser(input_2)
     assert parsed.parsed_input == "addresse mairie paris"
-    # To see what the function prints
-    # print(parsed.parsed_input)
-
-
-test_input_2(input_2)
